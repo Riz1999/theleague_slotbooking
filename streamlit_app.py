97fc07acde3570203230d64f6b994d39faf13ef5
rizwan zhad <92221232+Riz1999@users.noreply.github.com>
2023-08-16 13:02:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,12 @@ from googleapiclient.discovery import build
 import pandas as pd
 import pytz
 from datetime import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import csv
+import uuid
+from datetime import datetime, time
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
 SPREADSHEET_ID = "14afCQaLe9KOHw5KPQupqWwCQJUmthuUPg_Tvs4jbhXY"
@@ -66,45 +72,193 @@ def connect_to_gsheet():
     return gsheet_connector
 
 
-def slot_main():
+# def load_bookings_from_csv():
+#     bookings = []
+#     with open('bookings.csv', 'r') as csv_file:
+#         csv_reader = csv.DictReader(csv_file)
+#         for row in csv_reader:
+#             bookings.append(row)
+#     return bookings
 
-    col1, col2, col3 = st.columns([0.4,1,0.2])
-    with col2:
-        st.image("league.jpeg",width = 300)
 
-    col1, col2, col3 = st.columns([0.2,2,0.2])
+# def process_email_response(response):
+#     if "accept" in response.lower():
+#         return "Accepted"
+#     elif "decline" in response.lower():
+#         return "Declined"
+#     else:
+#         return "Unknown"
 
-    with col2:
-        st.title("Slot Booking for The League")
+# def update_csv_with_response(response, data):
+#     with open('responses.csv', 'a', newline='') as csvfile:
+#         fieldnames = ['Name', 'Sport', 'Slot', 'Response']
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#         writer.writerow({'Name': data['name'], 'Sport': data['sport'], 'Slot': data['slot'], 'Response': response})
 
-    gsheet_connector = connect_to_gsheet()
+
+
+def send_email_notification(name, mail_id, sport_type, slot_time,sport_type1,slot_time1):
+    # Email configuration
     
-    UTC = pytz.utc
-    IST = pytz.timezone('Asia/Kolkata')
-                
-    hr = str(datetime.now(IST).time())
     
-    if int(hr[0:2]) == 22 or int(hr[0:2]) == 23:
-        header2("Booking opens at 12AM")
+    # EMAIL_HOST = 'smtp.gmail.com'
+    # EMAIL_PORT = 587
+    # EMAIL_HOST_USER = 'woxsenlab@gmail.com'
+    # EMAIL_HOST_PASSWORD = 'tyddzpgkjhsgpeid'
+    # EMAIL_USE_TLS = True
+    
+     
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    smtp_username = "woxsenlab@gmail.com"
+    smtp_password = "gdjxsjefnyxyvkcn"
+    recipient_email = "rizwan.zhad@woxsen.edu.in"
+
+    # Email content
+    
+    
+
+    subject = f"Slot Approval: {name} has booked a slot"
+    body = f"Dear Deepanshi Gaur,\n\n{name} has booked a slot for {sport_type} or {sport_type1} at {slot_time} or {slot_time1}. Please review and approve the slot.\n\nBest regards,\nAI Research centre-Woxsen University"
+
+    # Create MIMEText and MIMEMultipart objects
+    msg = MIMEMultipart()
+    msg['From'] = smtp_username
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
         
-    else:
+    # Connect to the SMTP server and send email
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(smtp_username, recipient_email, msg.as_string())
+        st.success("Email notification sent successfully.")
+        
+    except Exception as e:
+        st.error(f"Failed to send email notification: {e}")
+    finally:
+        server.quit()
 
-        mail_id = st.text_input("Enter your woxsen Mail ID")
 
-        if len(mail_id) == 0 or "woxsen.edu.in" in mail_id:
+# @st.cache+
+# def handle_approval_decline_action(action, booking_id):
+#     bookings = load_bookings_from_csv()
+#     for booking in bookings:
+#         if booking['id'] == booking_id:
+#             booking['status'] = action
+#             save_bookings_to_csv(bookings)
+#             return True
+#     return False
 
-            name = st.text_input("Enter your Name")
-            contact = st.text_input("Enter your contact")
+# Endpoint for approving a booking
+# if st.experimental_get_query_params().get('action', [None])[0] == 'approve':
+#     booking_id = st.experimental_get_query_params().get('booking_id', [None])[0]
+#     if booking_id:
+#         if handle_approval_decline_action("approved", booking_id):
+#             st.success("Booking approved!")
+#         else:
+#             st.error("Booking not found.")
 
-            if len(name) != 0 and len(contact) != 0 and len(mail_id) != 0:
-                sports = ["-","Football pitch 1","Football pitch 2","Box Cricket","Basketball",
+# # Endpoint for declining a booking
+# if st.experimental_get_query_params().get('action', [None])[0] == 'decline':
+#     booking_id = st.experimental_get_query_params().get('booking_id', [None])[0]
+#     if booking_id:
+#         if handle_approval_decline_action("declined", booking_id):
+#             st.error("Booking declined.")
+#         else:
+#             st.error("Booking not found.")simulate_email_responses() 
+  
+def indoor(gsheet_connector,name,mail_id,contact):
+    sports = ["Select Your Ground","Table-Tennis 1", "Table-Tennis 2", "Table-Tennis 3","Badminton court-1","Badminton court-2","Badminton court-3","Badminton court-4","Badminton court-5","Badminton court-6","Badminton court-7","Badminton court-8","Squash-1","Squash-2"]
+    
+    sport_type1 = st.selectbox("Indoor",sports)
+    if sport_type1 != "Select Your Ground":
+
+                        df = get_data(gsheet_connector)
+                        time_df = df[df["Venue"] == sport_type1]
+
+                        booked = list(time_df["Slot Timing"])
+
+                        all_slots = []
+
+                        UTC = pytz.utc
+                        IST = pytz.timezone('Asia/Kolkata')
+
+                        hr = str(datetime.now(IST).time())
+
+                        if int(hr[0:2]) == 23:
+                            header2("Booking opens at 12AM")
+
+                        else:
+                            # Generate time slots from 5 AM to 11 AM
+                            for i in range(5, 8):
+                                x = "{:02d}:00 - {:02d}:00".format(i, i + 1)
+                                all_slots.append(x)
+
+                            new_slots = ["-"]
+
+                            for s in all_slots:
+                                if s not in booked:
+                                    new_slots.append(s)
+
+                            #del_slots = []
+                            
+                            # Generate time slots from 4 PM to 11 PM
+                            for i in range(16, 23):
+                                x = "{:02d}:00 - {:02d}:00".format(i, i + 1)
+                                all_slots.append(x)
+
+                            new_slots = ["-"]
+
+                            for s in all_slots:
+                                if s not in booked:
+                                    new_slots.append(s)
+
+                #if len(new_slots) == 1:
+                # header2("No Slots Available")
+
+                            # for i in range(16, 24):
+                            #     x = "{:02d}:00 - {:02d}:00".format(i, i + 1)
+                            #     del_slots.append(x)
+
+                            # for i in del_slots:
+                            #     if i in new_slots:
+                            #         new_slots.remove(i)
+
+
+                            if len(new_slots) == 1:
+                                header2("No Slots Available")
+
+                            else:
+                                slot_time1 = st.selectbox("Choose your time slot", new_slots)
+
+                                if slot_time1 != "-":
+                                    if st.button("Submit"):
+                                        add_row_to_gsheet(
+                                            gsheet_connector, [[name, mail_id, contact, sport_type1, slot_time1]]
+                                        )
+                                        header2("Your slot has been booked!")
+                                        st.success(" **Take a Screenshot of the slot details** ")
+                                        st.write("**Name:**",name)
+                                        st.write("**Venue:**", sport_type1)
+                                        st.write("**Slot Time:**", slot_time1)
+                                        
+                                        
+                                        send_email_notification(name, mail_id,"","", sport_type1, slot_time1)
+                            st.button("refresh", key="refresh_button")
+            
+                
+
+
+
+def outdoor(gsheet_connector, name,mail_id,contact):
+    sports = ["Select Your Ground","Football pitch 1","Football pitch 2","Box Cricket","Basketball",
                           "Sand Volleyball","Volleyball Court 1","Volleyball Court 2",
                           "Lawn Tennis Court 1","Lawn Tennis Court 2","Kabaddi","Golf","Croquet"]
-                sports.sort()
-                sport_type = st.selectbox("Choose your Venue",sports)
-
-
-                if sport_type != "-":
+    sport_type = st.selectbox("Outdoor",sports)
+    if sport_type != "Select Your Ground":
 
                     df = get_data(gsheet_connector)
                     time_df = df[df["Venue"] == sport_type]
@@ -149,7 +303,7 @@ def slot_main():
                             slot_time = st.selectbox("Choose your time slot", new_slots)
 
                             if slot_time != "-":
-                                if st.button("Book Slot"):
+                                if st.button("Submit"):
                                     add_row_to_gsheet(
                                         gsheet_connector, [[name, mail_id, contact, sport_type, slot_time]]
                                     )
@@ -158,15 +312,65 @@ def slot_main():
                                     st.write("**Name:**",name)
                                     st.write("**Venue:**", sport_type)
                                     st.write("**Slot Time:**", slot_time)
-                                    st.info("Click refresh before choosing different time slot")
-                        st.button("refresh")
+                                    
+                                    send_email_notification(name, mail_id,sport_type, slot_time,"","")
+                            st.button("refresh")
 
 
+
+
+
+def slot_main():
+
+    col1, col2, col3 = st.columns([0.4,1,0.2])
+    with col2:
+        st.image("league.jpeg",width = 300)
+
+    col1, col2, col3 = st.columns([0.2,2,0.2])
+
+    with col2:
+        st.title("Slot Booking for The League")
+
+    gsheet_connector = connect_to_gsheet()
+    
+    UTC = pytz.utc
+    IST = pytz.timezone('Asia/Kolkata')
+
+    current_time = datetime.now(IST).time()
+    
+           
+    hr = str(datetime.now(IST).time())
+    
+    if int(hr[0:2]) == 22 or int(hr[0:2]) == 23:
+        header2("Booking opens at 12AM")
+
+    else:
+
+        mail_id = st.text_input("Enter your woxsen Mail ID")
+
+        if len(mail_id) == 0 or "woxsen.edu.in" in mail_id:
+
+            name = st.text_input("Enter your Name")
+            contact = st.text_input("Enter your contact")
+            games = ["Select your Venue","Indoor", "Outdoor"]
+            venue = st.selectbox("Venue",games)
+            
+            if len(name) != 0 and len(contact) != 0 and len(mail_id) != 0 and venue =="Indoor":
+                indoor(gsheet_connector, name,mail_id,contact)
+               
+            elif len(name) != 0 and len(contact) != 0 and len(mail_id) != 0 and venue =="Outdoor":
+                outdoor(gsheet_connector, name,mail_id,contact)
         else:
-            st.error("You are not allowed to book a slot. Please enter woxsen mail ID")
+            st.error("You are not allowed to book a slot. Please enter woxsen mail ID")  
+
+
 
 
 
 if __name__ == "__main__":
     st.set_page_config(page_title="The League: Slot Booking", layout="centered")
+   
     slot_main()
+
+
+
